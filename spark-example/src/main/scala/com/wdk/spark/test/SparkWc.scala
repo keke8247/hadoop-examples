7697bb303bceee4cfeda5904d1ac6c8e6600efb9
wdk <wdk@erongdu.com>
2020-03-03 18:32:40 +0800

    spark

---
+++ spark-example/src/main/scala/com/wdk/spark/test/SparkWc.scala
@@ -0,0 +1,29 @@
+package com.wdk.spark.test
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @Description
+  * @Author wangdk,wangdk@erongdu.com
+  * @CreatTime 2020/3/3 14:52
+  * @Since version 1.0.0
+  */
+object SparkWc {
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark-Wc")
+
+        //定义spark环境
+        val sparkContext = new SparkContext(sparkConf)
+
+        //第二个参数 可以设置并行度 指定最小分区数
+        val inputData = sparkContext.textFile("spark-example/in",6)
+
+        inputData.saveAsTextFile("spark-example/output")
+
+        val wordCount = inputData.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+
+        wordCount.foreach(println)
+
+    }
+}
