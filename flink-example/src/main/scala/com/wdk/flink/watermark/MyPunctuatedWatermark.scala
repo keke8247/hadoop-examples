c748be5edff295434dd3d528c6e1eddb2fe7e476
wangdk <wangdk@erongdu.com>
2020-03-08 10:02:29 +0800

    定义PunctuatedWatermarks

--- flink-example/src/main/scala/com/wdk/flink/watermark/MyPunctuatedWatermark.scala
+++ flink-example/src/main/scala/com/wdk/flink/watermark/MyPunctuatedWatermark.scala
@@ -7,7 +7,7 @@ import org.apache.flink.streaming.api.watermark.Watermark
 /**
   * @Description:
   *              非周期的watermark
-  *              有监控到数据 决定watermark的生成
+  *              有监控到数据 决定watermark的生成 (数据触发)
   * @Author:wang_dk
   * @Date:2020 /3/1 0001 11:59
   * @Version: v1.0
@@ -16,9 +16,9 @@ class MyPunctuatedWatermark extends AssignerWithPunctuatedWatermarks[SensorReadi
     val bond : Long = 60*1000 //延时1分钟
 
 
-    override def checkAndGetNextWatermark(t: SensorReading, l: Long) = {
+    override def checkAndGetNextWatermark(t: SensorReading, extractedTimestamp: Long) = {
         if(t.id == "sensor_1"){ //SensorReading id为 sensor_1的数据进来 生成watermark
-            new Watermark(l-bond)
+            new Watermark(extractedTimestamp-bond)
         }else{
             null
         }
