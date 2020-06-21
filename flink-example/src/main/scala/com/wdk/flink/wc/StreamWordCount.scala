66eb32b9732d1740f2bdd12ae4e9d6ac7ce0272e
wangdk <wangdk@erongdu.com>
2020-06-21 10:09:17 +0800

    test

--- flink-example/src/main/scala/com/wdk/flink/wc/StreamWordCount.scala
+++ flink-example/src/main/scala/com/wdk/flink/wc/StreamWordCount.scala
@@ -1,5 +1,7 @@
 package com.wdk.flink.wc
 
+import org.apache.flink.runtime.state.StateBackend
+import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 
@@ -18,7 +20,6 @@ object StreamWordCount {
         //可以禁止掉 operator chains 优化. 当一个算子 执行了太复杂的操作.再使用此优化 可能不能最大限度利用slot的并行度. 可以使用disable 禁用掉.
 //        env.disableOperatorChaining();
 
-
         //创建数据流
         val intputStream = env.socketTextStream("master",7777)
 
@@ -28,7 +29,7 @@ object StreamWordCount {
             .startNewChain()    //断开前面的operator chains  重开一个chains
             .map((_,1))
             .keyBy(0)   //把元组中第一个元素作为key
-            .timeWindow(Time.seconds(5))
+//            .timeWindow(Time.seconds(5))
             .sum(1)     //根据元组中第二个元素求和
 
         wordCountDataStream
